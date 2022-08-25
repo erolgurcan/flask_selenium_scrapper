ec6f1f4825fcb1094b0c9dc565a9f670517046fd
Erol Gurcan <59280972+erolgurcan@users.noreply.github.com>
2022-08-25 11:02:11 -0700

    asd

--- flask_api.py
+++ flask_api.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def home():
     return "Flask heroku app"
 
+@app.route('/home')
+def asd():
+    print("asd")
+    return "done"
+
 if __name__ == "__main__":
     app.run()
